56601a957386df92392aa3e31167be4a9876d226
cosmos123E <puritysang180@gmail.com>
2024-11-23 15:19:42 +0300

    Initial commit: Added pythonassignment1

---
+++ pythonassignment1.py
@@ -0,0 +1,23 @@
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the operation based on user input
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation."
+
+# Display the result
+print(f"The result of {num1} {operation} {num2} = {result}")
+
